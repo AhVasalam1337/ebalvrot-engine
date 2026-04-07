f26fbea957bace117ac7431f1e20671ab4c3751c
AhVasalam1337 <64683917+AhVasalam1337@users.noreply.github.com>
2026-04-08 02:40:40 +0300

    IDE Sync: calculator.py

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def calculator():
+    print("--- Ebalvrot Engine: Калькулятор v1.0 ---")
+    try:
+        num1 = float(input("Введите первое число: "))
+        op = input("Введите операцию (+, -, *, /): ")
+        num2 = float(input("Введите второе число: "))
+
+        if op == '+': result = num1 + num2
+        elif op == '-': result = num1 - num2
+        elif op == '*': result = num1 * num2
+        elif op == '/': 
+            result = num1 / num2 if num2 != 0 else "Ошибка: деление на ноль"
+        else: result = "Неверная операция"
+        
+        print(f"Результат: {result}")
+    except ValueError:
+        print("Ошибка: введите корректные числа.")
+
+if __name__ == "__main__":
+    calculator()
